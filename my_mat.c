39e9eb739cb5c2692f533d05dd273aecfcb9d45d
ErgaDN <erga932@gmail.com>
2022-11-29 14:32:46 +0200

    first commit

---
+++ my_mat.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void getForMat (){
+    int arr[10][10];
+    for (int i = 0; 1 < 10 ; i++) {
+        for (int j = 0 ; j < 10 ; j++) {
+            scanf("%d" , &arr[i][j]);
+        }
+    }
+}
+   
+
