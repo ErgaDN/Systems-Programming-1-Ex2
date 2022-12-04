c8ab30b261e010f72b2f918e0e7ae1fdcedcbba6
ErgaDN <erga932@gmail.com>
2022-12-04 19:51:48 +0200

    adding space in scanf

--- main.c
+++ main.c
@@ -3,24 +3,24 @@
 
 int main() {
     char chooseMethod = ' ';
-    scanf("%c", &chooseMethod);
+    scanf(" %c", &chooseMethod);
     while (chooseMethod != 'D') {
         if (chooseMethod == 'A') {
             getForMat();
         } 
         else if (chooseMethod == 'B') {
             int i, j;
-            scanf("%d", &i);
-            scanf("%d", &j);
+            scanf(" %d", &i);
+            scanf(" %d", &j);
             routeExists(i, j);
         } 
         else if (chooseMethod == 'C') {
             int i, j;
-            scanf("%d", &i);
-            scanf("%d", &j);
+            scanf(" %d", &i);
+            scanf(" %d", &j);
             shortRoute(i, j);
         }
-        scanf("%c", &chooseMethod);
+        scanf(" %c", &chooseMethod);
     }
 
     return 0;
--- my_mat.c
+++ my_mat.c
@@ -8,10 +8,10 @@ int arr[10][10] = {0};
 void getForMat (){
     for (int i = 0; i < 10; i++) {
         for (int j = 0 ; j < 10 ; j++) {
-            scanf("%d" , &arr[i][j]);
+            scanf(" %d" , &arr[i][j]);
         }
     }
-    
+
     floydWarshall();
 }
    
